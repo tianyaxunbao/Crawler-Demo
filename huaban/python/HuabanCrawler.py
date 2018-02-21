e1b997c61b11871fa414782a50d0b1bbaec5dabb
tianyaxunbao <tianyaxunbao@gmail.com>
2018-02-21 18:09:09 +0800

    Update HuabanCrawler.py

--- huaban/python/HuabanCrawler.py
+++ huaban/python/HuabanCrawler.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 # encoding: utf-8
-# @author: cc <chai_pengfei@163.com>
+# @author: cc <chai_pengfei@163.com>，samuel
+
 
 import requests
 import re
-import os
-import os.path
+
 
 class HuabanCrawler():
     """ 抓去花瓣网上的图片 """
@@ -14,8 +14,7 @@ class HuabanCrawler():
         """ 在当前文件夹下新建images文件夹存放抓取的图片 """
         self.homeUrl = "http://huaban.com/favorite/beauty/"
         self.images = []
-        if not os.path.exists('./images'):
-            os.mkdir('./images')
+ 
 
     def __load_homePage(self):
         """ 加载主页面 """
@@ -32,6 +31,7 @@ class HuabanCrawler():
     def __process_data(self, htmlPage):
         """ 从html页面中提取图片的信息 """
         prog = re.compile(r'app\.page\["pins"\].*')
+        htmlPage = htmlPage.decode('utf-8')
         appPins = prog.findall(htmlPage)
         # 将js中的null定义为Python中的None
         null = None
@@ -53,28 +53,31 @@ class HuabanCrawler():
         """ 保存图片 """
         with open(imageName, 'wb') as fp:
             fp.write(content)
-
+        file = drive.CreateFile({'title': imageName})
+        file.SetContentFile(imageName)
+        file.Upload() # Files.insert()
+ 
     def get_image_info(self, num=20):
         """ 得到图片信息 """
         self.__process_data(self.__load_homePage())
-        for i in range((num-1)/20):
+        for i in range((num-1)//20):
             self.__process_data(self.__load_more(self.images[-1]['id']))
         return self.images
 
     def down_images(self):
         """ 下载图片 """
-        print "{} image will be download".format(len(self.images))
+        
         for key, image in enumerate(self.images):
-            print 'download {0} ...'.format(key)
+            print ('download {0} ...'.format(key))
             try:
                 req = requests.get(image["url"])
             except :
-                print 'error'
-            imageName = os.path.join("./images", image["id"] + "." + image["type"])
+                print('error') 
+            imageName = image["id"] + "." + image["type"]
             self.__save_image(imageName, req.content)
 
 
-if __name__ == '__main__':
-    hc = HuabanCrawler()
-    hc.get_image_info(200)
-    hc.down_images()
+
+hc = HuabanCrawler()
+hc.get_image_info(200)
+hc.down_images()
